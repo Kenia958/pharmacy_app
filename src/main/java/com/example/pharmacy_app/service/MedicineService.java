2c5ecc88a9400f0cb377fb7ce0485199744ff388
Kenia958 <keniaigiraneza@gmail.com>
2025-05-03 12:56:00 +0100

    Create MedicineService.java

---
+++ src/main/java/com/example/pharmacy_app/service/MedicineService.java
@@ -0,0 +1,26 @@
+package com.example.pharmacy_app.service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.pharmacy_app.model.Medicine;
+import com.example.pharmacy_app.repository.MedicineRepository;
+
+import java.util.List;
+
+@Service
+public class MedicineService {
+    private final MedicineRepository medicineRepository;
+
+    public MedicineService(MedicineRepository medicineRepository) {
+        this.medicineRepository = medicineRepository;
+    }
+
+    public List<Medicine> searchMedicines(String query) {
+        return medicineRepository.findByNameContainingIgnoreCase(query);
+    }
+
+    public Medicine addMedicine(Medicine medicine) {
+        return medicineRepository.save(medicine);
+    }
+}
